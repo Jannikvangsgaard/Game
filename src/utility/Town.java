3cea93f29d4491e945ddb3efe46e93a1d4fdb388
computer <computer@computer-Pc.P-2601HN-F1>
2013-01-11 09:36:11 +0100

    Town

--- src/utility/Town.java
+++ src/utility/Town.java
@@ -19,6 +19,18 @@ public class Town {
     public int getGold(){
         return totalGold;
     }
+    public int getLInf(){
+        return soldiers[0];
+    }
+    public int getHInf(){
+        return soldiers[1];
+    }
+    public int getLCav(){
+        return soldiers[2];
+    }
+    public int getHCav(){
+        return soldiers[3];
+    }
 
     public Boolean convertSoldiers(int[] sol) {
         int tot = 0;
@@ -67,24 +79,49 @@ public class Town {
     public void build(int building){
         switch (building){
             case 1:
+                if (totalGold>=10){
                 this.building.setBarrack(true);
+                totalGold-=10;
+                }
                 break;
             case 2:
+                if (totalGold>=50){
                 this.building.setStable(true);
+                totalGold-=50;
+                }
                 break;
             case 3:
+                if (totalGold>=80){
                 this.building.setWall(true);
+                totalGold-=80;
+                }
                 break;
             case 4:
+                if (totalGold>=250){
                 this.building.setGreatWall(true);
+                totalGold-=250;
+                }
                 break;
             case 5:
-                this.building.setSpecialWeaponSmith(true);
-                break;
+                if (totalGold>=80){
+                    this.building.setMarkte(true);
+                    totalGold-=80;
+                   break;
+                }
             case 6:
+                if (totalGold>=130){
+                this.building.setSpecialWeaponSmith(true);
+                totalGold-=130;
+                }
+                break;
+            case 7:
+                if (totalGold>=300){
                 this.building.setSiegeEngineHouse(true);
+                totalGold-=300;
+                }
                 break;
         }
     }
     
 }
+
