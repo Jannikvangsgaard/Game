2ebe8a362a25d74dbf65cc8a58e77e7dc699af94
Mark Andreas Baagø <Mabaagoe1_4@msn.com>
2013-01-22 12:02:47 +0100

    hej

--- src/utility/Army.java
+++ src/utility/Army.java
@@ -7,17 +7,16 @@ import java.io.Serializable;
  * @author Jannik
  */
 public class Army implements Serializable {
-    
-    
+
     private boolean onlyCav = false;
     private int[] army;
-    private int arrival, home=2;
+    private int arrival, home = 2;
     private double damage;
     private int win;
-    
-    public Army(int[] army){
-       double amount = army[0];
-       this.army = army;
+
+    public Army(int[] army) {
+        double amount = army[0];
+        this.army = army;
         damage = amount;
         amount = army[1];
         damage += 1.5 * amount;
@@ -25,46 +24,46 @@ public class Army implements Serializable {
         damage += 1.5 * amount;
         amount = army[3];
         damage += 2.5 * amount;
-        if (ifOnlyCav(army)){
-            arrival =1;
-        }else{
-        arrival = 2;
+        if (ifOnlyCav(army)) {
+            arrival = 1;
+        } else {
+            arrival = 2;
         }
-        
+
     }
-    
-    public int win(){
+
+    public int win() {
         return win;
     }
-    
-    public void winner(int win){
+
+    public void winner(int win) {
         this.win = win;
     }
-    
-    public double getDamage(){
+
+    public double getDamage() {
         return damage;
     }
-    
-    public boolean ifOnlyCav(int[] army){
-        if (army[0]==0&&army[1]==0){
+
+    public boolean ifOnlyCav(int[] army) {
+        if (army[0] == 0 && army[1] == 0) {
             boolean onlyCav = true;
             arrival = 1;
             return true;
         }
         return false;
     }
-    public boolean isHome(){
-      if (home==0){
-          return true;
-      }
+
+    public boolean isHome() {
+        if (home == 0) {
+            return true;
+        }
         return false;
-        
+
     }
-    
+
     public boolean isOnlyCav() {
         return onlyCav;
     }
-
 
     public int[] getArmy() {
         return army;
@@ -79,15 +78,14 @@ public class Army implements Serializable {
     }
 
     public void move() {
-     if (arrival>0){
-         arrival--;
-     } else if (arrival>0&&home!=0){
-         home--;
-     }
-    }
-    public String toString(){
-        return "Arrival: " + arrival + " your army is home in " + home + " turn(s)\n";
+        if (arrival > 0) {
+            arrival--;
+        } else if (arrival < 0 && home != 0) {
+            home--;
+        }
     }
 
-    
+    public String toString() {
+        return "Arrival: " + arrival + " your army is home in " + home + " turn(s)\n";
+    }
 }
